76ea26f4ecb912f7cbb1e24df6f067ae711a4fd2
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-17 20:27:27 -0300

    Add project for tests

---
+++ GameTests/Player.cs
@@ -0,0 +1,16 @@
+namespace GameTests
+{
+    public class PlayerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Constructor_()
+        {
+            Assert.Pass();
+        }
+    }
+}
