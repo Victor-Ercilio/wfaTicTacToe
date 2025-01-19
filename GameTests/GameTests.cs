4847905ff24329993516d7f2b6adaf92827a8e41
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-19 08:05:14 -0300

    Add test: state is initialize when game is built

--- GameTests/GameTests.cs
+++ GameTests/GameTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicTacToe;
 using RealGame = TicTacToe.Game;
 using RealPlayer = TicTacToe.Player;
 
@@ -27,6 +28,18 @@ namespace GameTests
 
             Assert.IsNotNull(game);
             Assert.IsInstanceOf<RealGame>(game);
+        }
+
+        [Test]
+        public void GameStateInitialize_PlayerMakeMove_ThrowsIllegalOperationException()
+        {
+            RealGame game = new RealGame(PlayerOne, PlayerTwo);
+            RealGame.EState actual;
+            RealGame.EState expected = RealGame.EState.Initialize;
+
+            actual = game.State;
+
+            Assert.AreEqual(expected, actual);
         }
 
        
