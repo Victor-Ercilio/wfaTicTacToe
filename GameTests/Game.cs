a1258543cd38d9c93cd5f1c125c9094f3a4ec32e
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-18 22:29:16 -0300

    Add test for Constructor of Game

---
+++ GameTests/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealGame = TicTacToe.Game;
+using RealPlayer = TicTacToe.Player;
+
+namespace GameTests
+{
+    internal class Game
+    {
+        private readonly RealPlayer PlayerOne = new RealPlayer("Foo", 'F');
+        private readonly RealPlayer PlayerTwo = new RealPlayer("Bar", 'B');
+
+        [Test]
+        public void Constructor_TwoDifferentPlayers_ReturnsInstance()
+        {
+            RealGame game;
+
+            game = new RealGame(PlayerOne, PlayerTwo);
+
+            Assert.IsInstanceOf<RealGame>(game);
+        }
+
+       
+    }
+}
