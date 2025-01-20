7445c34af060d712dff584b29b6881ac2fce9fcf
Victor Ercilio Akeno Yamane <victor.ercilio@hotmail.com>
2025-01-20 09:03:24 -0300

    Add Board

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace TicTacToe
 {
-    internal class Board
+    public class Board
     {
+        protected char[,] board = new char[3,3];
+
+        public char this[int i, int j]
+        {
+            get
+            {
+                return board[i,j];
+            }
+            set
+            {
+                board[i,j] = value;
+            }
+        }
+
+        public char this[Area v, Area h]
+        {
+            get
+            {
+                return board[(int)v, (int)h];
+            }
+            set
+            {
+                board[(int)v, (int)h] = value;
+            }
+        }
+
+        public enum Area
+        {
+            Top = 0,
+            Center = 1,
+            Bottom = 2,
+            Left = Top,
+            Right = Bottom,
+        }
     }
 }
